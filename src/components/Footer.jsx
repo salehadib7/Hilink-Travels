68d948593eea71db1fee9cc35aaf9a3c74b4abb2
salehadib7 <salehadib007@gmail.com>
2024-10-30 20:40:24 +0600

    Version 1.0.2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 const Footer = () => {
     return (
-      <footer className="flexCenter mb-10">
+      <footer className="flexCenter mb-10">  
         <div className="padding-container max-container flex w-full flex-col gap-14">
           <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
             <Link href="/" className="mb-10">
@@ -14,7 +14,7 @@ const Footer = () => {
   
             <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
               {FOOTER_LINKS.map((columns) => (
-                <FooterColumn title={columns.title}>
+                <FooterColumn key={columns.title} title={columns.title}>
                   <ul className="regular-14 flex flex-col gap-4 text-gray-30">
                     {columns.links.map((link) => (
                       <Link href="/" key={link}>
